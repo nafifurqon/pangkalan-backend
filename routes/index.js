b08acea695a8e280e7512f5b509bac9c9fac46c5
nafifurqon <muhammadnafi98@gmail.com>
2021-09-18 17:15:11 +0700

    init app

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const router = express.Router();
+
+/* GET home page. */
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'Hello World!',
+  });
+});
+
+module.exports = router;

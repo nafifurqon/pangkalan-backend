1d095f3f6fb8bf1784060a5f61c90ef0a305ee5b
nafifurqon <muhammadnafi98@gmail.com>
2021-09-29 10:21:56 +0700

    chore: added models and migration for create status, transaction, and history tables

---
+++ migrations/20210928152143-create-transaction.js
@@ -0,0 +1,45 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      do_number: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      seller: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: 'user_profiles',
+          key: 'id',
+          onDelete: 'cascade',
+          onUpdate: 'cascade',
+        },
+      },
+      customer: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      quantity: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('transactions');
+  },
+};
---
+++ models/history.js
@@ -0,0 +1,49 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class History extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      const { Transaction } = models;
+      this.belongsTo(Transaction, { foreignKey: 'transaction_id', as: 'transaction' });
+      this.belongsTo(Transaction, { foreignKey: 'status', as: 'status' });
+    }
+  }
+  History.init({
+    transaction_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'transactions',
+        key: 'id',
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+    },
+    status: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'statuses',
+        key: 'id',
+        onDelete: 'cascade',
+        onUpdate: 'cascade',
+      },
+    },
+    history_date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'History',
+    tableName: 'histories',
+  });
+  return History;
+};
